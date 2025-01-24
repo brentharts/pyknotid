2c4a9028a0b3e156f07b85b46867e969ad672124
brent hartshorn <brenthartshorn@proton.me>
2025-01-24 13:31:42 -0800

    option --test

--- knotid.py
+++ knotid.py
@@ -25,3 +25,21 @@ import pyknotid.spacecurves as sp
 print(sp)
 import pyknotid.make as mk
 print(mk)
+
+def test():
+	k = sp.SpaceCurve(mk.trefoil())
+	print(k)
+	g2 = k.gauss_code(recalculate=True, try_cython=False)
+	print(g2)
+
+def test_random():
+	from random import uniform
+	points = [ [uniform(-1,1),uniform(-1,1),uniform(-1,1)] for i in range(10)]
+	k = sp.SpaceCurve( points )
+	print(k)
+	g2 = k.gauss_code(recalculate=True, try_cython=False)
+	print(g2)
+
+if '--test' in sys.argv:
+	test()
+	test_random()
